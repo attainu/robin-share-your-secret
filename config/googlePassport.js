a55f62474d3decb659c786f026d7bb365b455925
Rinki Yadav <rinkiyadav15me@gmail.com>
2020-06-19 23:55:12 +0530

    some modification is done in previous coding

--- config/googlePassport.js
+++ config/googlePassport.js
@@ -6,8 +6,8 @@ const User = require("../model/user");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
