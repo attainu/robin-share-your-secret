ee40a57b2b1c32e94128ddc27416c0133a697b2c
farukEncoded <informfaruk@gmail.com>
2020-06-26 10:50:15 +0530

    Added npm run script to package.json

--- model/user.js
+++ model/user.js
@@ -7,8 +7,8 @@ const userSchema = new mongoose.Schema({
   password: String,
   googleId: String,
   secret: String,
-  title:String,
-  facebookId:String
+  title: String,
+  facebookId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
