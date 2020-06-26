d318cd596b1b9e0d8b18d8ebf2c0bc6d2d738619
Rinki Yadav <rinkiyadav15me@gmail.com>
2020-06-26 14:23:27 +0530

    changes done

--- model/user.js
+++ model/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
-  secret: String,
+  secret: [String],
   title:String,
   facebookId:String
 });
